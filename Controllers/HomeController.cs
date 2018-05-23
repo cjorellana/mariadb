46b90ed5220eb4990cd1f4a3f83bc4b6b28038c2
corellanaUG <corellana@galileo.edu>
2018-05-23 11:41:34 -0600

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MariaDBTest.Models;
+using Microsoft.AspNetCore.Authorization;
+using MariaDBTest.Data;
+
+namespace MariaDBTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //[Authorize]
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            //using (var context = new LibraryContext)
+            //{
+            //    var books = context.Book;
+            //} 
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MariaDBTest.Models
+{
+    public class Book
+    {
+        [Key]
+        public string ISBN { get; set; }
+        [Display(Name = "Titulo del Libro")]
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Language { get; set; }
+        public int Pages { get; set; }
+        public virtual Publisher Publisher { get; set; }
+    }
+}
